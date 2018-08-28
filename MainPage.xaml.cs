46cce2fd37ba1e3d566ad8fab080d814ec463eef
John Gallardo <jgalla@microsoft.com>
2018-08-28 14:32:52 -0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking;
+using Windows.System.RemoteSystems;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CapabilityChecker
+{
+    public class PageModel : PropertyChangedBase
+    {
+        public PageModel()
+        {
+            RemoteSystemModels = new ObservableCollection<RemoteSystemModel>();
+        }
+        public ObservableCollection<RemoteSystemModel> RemoteSystemModels { get; private set; }
+
+        private RemoteSystemModel _thisRemoteSystem;
+
+        public RemoteSystemModel ThisRemoteSystem
+        {
+            get => _thisRemoteSystem;
+            set => SetPropertyReference(nameof(ThisRemoteSystem), ref _thisRemoteSystem, value);
+        }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private PageModel _model = new PageModel();
+
+        public MainPage()
+        {
+            this.DataContext = _model;
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (_watcher == null)
+            {
+                _watcher = await CreateWatcherAsync();
+            }
+        }
+
+        private async Task<RemoteSystemWatcher> CreateWatcherAsync()
+        {
+            var access = await RemoteSystem.RequestAccessAsync();
+            if (access == RemoteSystemAccessStatus.Allowed)
+            {
+                var watcher = RemoteSystem.CreateWatcher();
+                watcher.RemoteSystemAdded += Watcher_RemoteSystemAdded;
+                watcher.Start();
+                return watcher;
+            }
+            return null;
+        }
+
+        private async void Watcher_RemoteSystemAdded(RemoteSystemWatcher sender, RemoteSystemAddedEventArgs args)
+        {
+            await Dispatcher.RunIdleAsync((_) =>
+            {
+                AddRemoteSystem(args.RemoteSystem);
+            });
+        }
+
+        void AddRemoteSystem(RemoteSystem system)
+        {
+            _model.RemoteSystemModels.Add(new RemoteSystemModel(system));
+        }
+
+        RemoteSystemWatcher _watcher;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+            var model = element?.DataContext as RemoteSystemModel;
+            await model?.CheckAppServiceCapableAsync();
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+            var model = element?.DataContext as RemoteSystemModel;
+            await model?.LaunchUri("jgtest://www.microsoft.com");
+        }
+
+        private async void LoadThisClicked(object sender, RoutedEventArgs e)
+        {
+            var access = await RemoteSystem.RequestAccessAsync();
+            if (access == RemoteSystemAccessStatus.Allowed)
+            {
+                var hn = new HostName("127.0.0.1");
+                var rs = await RemoteSystem.FindByHostNameAsync(hn);
+                _model.ThisRemoteSystem = new RemoteSystemModel(rs);
+            }
+        }
+    }
+}
---
+++ RemoteSystemModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.RemoteSystems;
+
+namespace CapabilityChecker
+{
+    public enum CapabilityState
+    {
+        NotChecked,
+        Checking,
+        Capable,
+        NotCapable
+    }
+
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void SetPropertyValue<T>(string name, ref T old, T n) where T : IComparable
+        {
+            if (old.CompareTo(n) != 0)
+            {
+                old = n;
+                NotifyPropertyChanged(name);
+            }
+        }
+
+        protected void SetPropertyReference<T>(string name, ref T old, T n) where T : class
+        {
+            if(old != n)
+            {
+                old = n;
+                NotifyPropertyChanged(name);
+            }
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class RemoteSystemModel : PropertyChangedBase
+    {
+        public RemoteSystemModel(RemoteSystem system)
+        {
+            _remoteSystem = system;
+        }
+
+        public string Name { get { return _remoteSystem.DisplayName; } }
+
+        public string Kind { get { return _remoteSystem.Kind; } }
+
+        private CapabilityState _isAppServiceCapable = CapabilityState.NotChecked;
+        public CapabilityState IsAppServiceCapable
+        {
+            get => _isAppServiceCapable;
+            set => SetPropertyValue(nameof(IsAppServiceCapable), ref _isAppServiceCapable, value);
+        }
+
+        public async Task CheckAppServiceCapableAsync()
+        {
+            IsAppServiceCapable = CapabilityState.Checking;
+            var operation = _remoteSystem.GetCapabilitySupportedAsync(KnownRemoteSystemCapabilities.AppService);
+            IsAppServiceCapable = await operation ? CapabilityState.Capable : CapabilityState.NotCapable;
+        }
+
+        public async Task LaunchUri(string uri)
+        {
+            RemoteSystemConnectionRequest request = new RemoteSystemConnectionRequest(_remoteSystem);
+            await Windows.System.RemoteLauncher.LaunchUriAsync(request, new Uri(uri));
+        }
+
+        private RemoteSystem _remoteSystem;
+    }
+}
